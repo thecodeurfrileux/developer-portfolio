487462eb245eb9799b1c98a1468a3aa47b22d42b
titouan <titou.hellegouarch@gmail.com>
2026-06-30 12:23:27 +0200

    fix favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,11 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 export const metadata: Metadata = {
 	icons: {
 		icon: [
+			{ url: "/icons/dark-logo.png", type: "image/png", sizes: "64x64" },
 			{ url: "/icons/light-logo.png", media: "(prefers-color-scheme: light)" },
 			{ url: "/icons/dark-logo.png", media: "(prefers-color-scheme: dark)" },
 		],
-		apple: "/dark-logo.png",
+		apple: "/icons/dark-logo.png",
 	},
 };
 
